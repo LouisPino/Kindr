96e7d434a9d986a2b808116e2dd5198a83d2ca83
laurhimms <lauriehimmel1@gmail.com>
2023-09-06 15:54:12 -0700

    stashing code

--- frontend/src/pages/Welcome/index.jsx
+++ frontend/src/pages/Welcome/index.jsx
@@ -1,20 +1,26 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import { Link } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
+
+import("./welcome.css");
 
 export default function Welcome() {
   const { loginWithRedirect, logout, user, isLoading } = useAuth0();
 
-
-
-  return (<>
-    <h2>WELCOME PAGE</h2>
-
+  return (
+    <>
+      <section className="welcome-page">
+        <h2 className="h2-header kindr-header">Welcome!</h2>
+        <p className="body-font welcome-emphasize">
+          We're so glad you're here.
+        </p>
+        <p className="body-font welcome-body">
+          Kindr is an app where you can do good things and inspire others to do
+          good. We're just a bunch of do-gooders 'round these parts.
+        </p>
+      </section>
     </>
   );
 }
-
-
-
 
 // <header className="welcome">
 // {!isLoading && !user && (
