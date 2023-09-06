a0f7028c94651f3318d3fb1d74d05da73ea9e5f9
LouisPino <ljpino95@gmail.com>
2023-09-05 21:56:44 -0400

    remove cors

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
