87d4cf1ead08541dfac905dfca60545b5e75a006
laurhimms <lauriehimmel1@gmail.com>
2023-09-07 15:47:25 -0700

    Test route

--- backend/routes/challenges.js
+++ backend/routes/challenges.js
@@ -4,7 +4,10 @@ const challengeCtrl = require("../controllers/challenges")
 
 /* GET home page. */
 router.get('/', challengeCtrl.index);
-router.get('/daily', challengeCtrl.createDailyChallenge);
+// router.get('/daily', challengeCtrl.createDailyChallenge);
+router.get('/daily', (req, res) => {
+  res.send('test')
+});
 router.post('/', challengeCtrl.create);
 
 
