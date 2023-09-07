13e5a040c265d0b449570b8b9933dc4fe56fbc70
laurhimms <lauriehimmel1@gmail.com>
2023-09-06 17:20:02 -0700

    userData name

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -1,41 +1,75 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import { Link, useNavigate } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Link, useNavigate } from "react-router-dom";
 import { findUserByEmail, updateUser } from "../../utilities/user-service";
+import { getChallenges } from "../../utilities/challenge-service";
+import ChallengeList from "../Challenge/ChallengeList";
 
 export default function Dashboard() {
   const { user } = useAuth0();
-  const {userId} = useParams()
-  const [isLoading, setIsLoading] = useState(true)
-const navigate = useNavigate()
+  const { userId } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
 
+  const [challenges, setChallenges] = useState(null);
+
+
+
+  const [userData, setUserData] = useState({})
+  
   useEffect(()=>{
     if(user){
     async function fillUserObj(){
-    const userData = await findUserByEmail(user.email)
+       const retrievedUserData = await findUserByEmail(user.email)
+      setUserData(retrievedUserData)
     }  
     fillUserObj()
-    setIsLoading(false)
   }
     else{
       navigate('/')}
   }, [])
 
 
-  
-  return(
+
+  async function handleRequest() {
+    const challengeResponse = await getChallenges();
+
+    if (challengeResponse.length) {
+      console.log("challengersp", challengeResponse);
+      setChallenges(challengeResponse);
+    } else {
+      console.log(challengeResponse);
+      // context update for error handling might be called
+    }
+  }
+  useEffect(() => {
+    if (challenges) {
+      setIsLoading(false);
+    }
+  }, [challenges]);
+
+  useEffect(() => {
+    if (user) {
+      async function fillUserObj() {
+        const userData = await findUserByEmail(user.email);
+        await handleRequest();
+      }
+
+      fillUserObj();
+    } else {
+      navigate("/");
+    }
+  }, []);
+
+  return isLoading ? (
     <>
-        {isLoading && (
-      <>
-      <h1>LOADING</h1> 
+      <h1>LOADING</h1>
     </>
-    )}
-    {!isLoading && user && (
-        <>
-        <h1>{user.name || user.email.split("@")[0]}'s DASHBOARD</h1> 
-      </>
-      )}
-      </>
-    )
+  ) : (
+    <>
+      <h1>{userData.username}'s DASHBOARD</h1>
+      <ChallengeList challenges={challenges} />
+    </>
+  );
 }
