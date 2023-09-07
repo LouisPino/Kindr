e8fa39155a69bed4844be7f6282d6d294ccef4d1
LouisPino <ljpino95@gmail.com>
2023-09-07 19:47:29 -0400

    cors allow all

--- backend/server.js
+++ backend/server.js
@@ -26,13 +26,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(cors({
-//   origin: FRONTENDURL,
-//   methods: "GET, POST, PUT, DELETE",
-//   credentials: true,
-// }))
-
-app.use(cors()) 
+app.use(cors({
+  origin: FRONTENDURL,
+  methods: "GET, POST, PUT, DELETE",
+  credentials: true,
+}))
 
 
 
