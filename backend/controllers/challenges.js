b245c091c2bb4a979156cae8650ea32b339a2f73
laurhimms <lauriehimmel1@gmail.com>
2023-09-07 15:20:20 -0700

    undoing

--- backend/controllers/challenges.js
+++ backend/controllers/challenges.js
@@ -7,7 +7,6 @@ module.exports = {
   show,
   update,
   delete: destroy,
-  createDailyChallenge
 };
 
 async function create(req, res) {
@@ -51,29 +50,4 @@ async function destroy(req, res) {
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
-}
-
-async function createDailyChallenge(req, res, next) {
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content:
-          'Generate a good deed that someone could achieve in less than 24 hours. Please come up with a title for the deed and a description no longer than 4 sentences explaining what the deed is. It should be formatted like this: (Deed Title): (Deed Description). Do not use the words "title" or "description".',
-      },
-    ],
-    temperature: 0.7,
-    max_tokens: 500,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  });
-  const message = response.data.choices[0].message;
-  const dailyDeed = message.content;
-  const splitEm = dailyDeed.split(": ");
-  const newTitle = splitEm[0];
-  const newDeed = splitEm[1];
-  const deedObj = { title: newTitle, description: newDeed, daily: true };
-  Challenge.create(deedObj)
 }
