843888f315f7d0d8109c96f090fddb38c0fd7fb4
LouisPino <ljpino95@gmail.com>
2023-09-08 10:23:46 -0400

    testing api

--- backend/controllers/challenges.js
+++ backend/controllers/challenges.js
@@ -102,9 +102,10 @@ const response = await fetch("https://api.openai.com/v1/chat/completions", {
   "Access-Control-Allow-Credentials": true,},
   body: JSON.stringify(gptConfig)
 })
+console.log(response)
 res.json(response)
 }catch(err){
-  console.log(err)
+  console.log('err', err)
   res.status(400).json({ error: err.message });
 }
 }
