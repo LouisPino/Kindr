00fb0f58370702a4a610251a7a0c6e6b8be4c550
LouisPino <ljpino95@gmail.com>
2023-09-07 20:25:08 -0400

    testing manual call

--- backend/controllers/challenges.js
+++ backend/controllers/challenges.js
@@ -57,31 +57,31 @@ async function destroy(req, res) {
   }
 }
 
-async function createDailyChallenge(req, res, next) {
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content:
-          'Generate a good deed that someone could achieve in less than 24 hours. Please come up with a title for the deed and a description no longer than 4 sentences explaining what the deed is. It should be formatted like this: (Deed Title): (Deed Description). Do not use the words "title" or "description".',
-      },
-    ],
-    temperature: 0.7,
-    max_tokens: 500,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  });
-  const message = response.data.choices[0].message;
-  const dailyDeed = message.content;
-  const splitEm = dailyDeed.split(": ");
-  const newTitle = splitEm[0];
-  const newDeed = splitEm[1];
-  const deedObj = { title: newTitle, description: newDeed, daily: true };
-  Challenge.create(deedObj)
-  res.send(deedObj)
-}
+// async function createDailyChallenge(req, res, next) {
+//   const response = await openai.createChatCompletion({
+//     model: "gpt-3.5-turbo",
+//     messages: [
+//       {
+//         role: "system",
+//         content:
+//           'Generate a good deed that someone could achieve in less than 24 hours. Please come up with a title for the deed and a description no longer than 4 sentences explaining what the deed is. It should be formatted like this: (Deed Title): (Deed Description). Do not use the words "title" or "description".',
+//       },
+//     ],
+//     temperature: 0.7,
+//     max_tokens: 500,
+//     top_p: 1,
+//     frequency_penalty: 0,
+//     presence_penalty: 0,
+//   });
+//   const message = response.data.choices[0].message;
+//   const dailyDeed = message.content;
+//   const splitEm = dailyDeed.split(": ");
+//   const newTitle = splitEm[0];
+//   const newDeed = splitEm[1];
+//   const deedObj = { title: newTitle, description: newDeed, daily: true };
+//   Challenge.create(deedObj)
+//   res.send(deedObj)
+// }
 
 
 
@@ -91,7 +91,6 @@ let gptConfig={
   "messages": [{"role": "user", "content": "Say this is a test!"}],
   "temperature": 0.7
 }
-
 
 const response = await fetch("https://api.openai.com/v1/chat/completions", {
   method: "POST",
