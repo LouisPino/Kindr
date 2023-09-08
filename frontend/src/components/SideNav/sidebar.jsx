f03838417abb92f300592c4ad8b806d8b933b452
laurhimms <lauriehimmel1@gmail.com>
2023-09-08 07:18:59 -0700

    stashing code

---
+++ frontend/src/components/SideNav/sidebar.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './sidebar.css'
+import { NavLink } from 'react-router-dom'
+
+
+
+export default function Sidenav() {
+    const navData = [
+        {
+            id: 0,
+            text: "Dashboard",
+            link: "/"
+        },
+        {
+            id: 1,
+            text: "Profile",
+            link: "explore"
+        },
+        {
+            id: 2,
+            text: "Add A Challenge",
+            link: "statistics"
+        },
+    ]
+    
+    const [open, setopen] = useState(true)
+    const toggleOpen = () => {
+        setopen(!open)
+    }
+
+    return (
+      <div className={open? "sidenav" : "sidenavClosed"}>
+            <button className="menuBtn" onClick={toggleOpen}>{open? <p>LEFT</p>: <p>RIGHT</p>}
+    </button>
+    {navData.map(item =>{
+        return <NavLink key={item.id} className="sideitem">
+ {/* {item.icon} */}
+ <span className={open? "linkText" : "linkTextClosed"}>{item.text}</span>
+ </NavLink>
+        })}
+      </div>
+    )
+  }
