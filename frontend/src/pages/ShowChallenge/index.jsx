e2ffcd6fbfd0ac4fcba94e6bf5505e96f7ad4d5d
LouisPino <ljpino95@gmail.com>
2023-09-08 18:58:13 -0400

    display users who have completed a challenge on show page

--- frontend/src/pages/ShowChallenge/index.jsx
+++ frontend/src/pages/ShowChallenge/index.jsx
@@ -1,71 +1,99 @@
-import { useNavigate, useParams } from "react-router"
+import { useNavigate, useParams } from "react-router";
 import { findChallengesByIds } from "../../utilities/challenge-service";
 import { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { findUserByEmail, updateUser } from "../../utilities/user-service";
+import {
+  findUserByEmail,
+  findUsersByCompletedChalleneges,
+  updateUser,
+} from "../../utilities/user-service";
 
 export default function ShowChallenge() {
-    const { loginWithRedirect, logout, user} = useAuth0();
-    const navigate = useNavigate()
-const { id } = useParams()
-const [challenge, setChallenge] = useState({})
-const [userData, setUserData] = useState({})
-  useEffect(()=>{
-    if(user){
-    async function fillUserObj(){
-      const retrievedUserData = await findUserByEmail(user.email)
-      setUserData(retrievedUserData)
+  const { loginWithRedirect, logout, user } = useAuth0();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [challenge, setChallenge] = useState({});
+  const [userData, setUserData] = useState({});
+  const [completedUsers, setCompletedUsers] = useState([]);
+  useEffect(() => {
+    if (user) {
+      async function fillUserObj() {
+        const retrievedUserData = await findUserByEmail(user.email);
+        setUserData(retrievedUserData);
+      }
+      fillUserObj();
+    } else {
+      navigate("/");
     }
-    fillUserObj()
-  }
-else{
-  navigate('/')
-}
-  }, [])
+  }, []);
 
   async function addComplete(e) {
-    e.preventDefault()
-    let userChallenges = userData.completedChallenges
+    e.preventDefault();
+    let userChallenges = userData.completedChallenges;
 
-    userChallenges.push(e.target.id)
+    userChallenges.push(e.target.id);
 
-    const newUserData = {...userData, [e.target.name]: userChallenges}
-    console.log('newUserData', newUserData)
+    const newUserData = { ...userData, [e.target.name]: userChallenges };
+    console.log("newUserData", newUserData);
 
-    updateUser(newUserData)
-    setUserData(newUserData)
-    console.log('user', user)
+    updateUser(newUserData);
+    setUserData(newUserData);
+    console.log("user", user);
   }
+  useEffect(() => {
+    async function getChallenge() {
+      const ids = [id];
+      const gotChallenge = await findChallengesByIds(ids);
+      setChallenge(gotChallenge[0]);
+      const gotUsers = await findUsersByCompletedChalleneges(id);
+      setCompletedUsers(gotUsers);
+    }
+    getChallenge();
+  }, []);
 
-useEffect(()=>{
-   async function getChallenge(){
-const ids = [id]
-const gotChallenge = await findChallengesByIds(ids)
-setChallenge(gotChallenge[0])
-   }
-   getChallenge()
-},[])
-
-    return (
-    <div className="challenge-block" key={challenge._id}>
-    <img
-      className="challenge-picture"
-      src="https://res.cloudinary.com/dpsymdmyi/image/upload/v1692365554/bebpthuftxhz18roqbwl.png"
-    />
-    <h3 className="h3-challenge h3-header kindr-header">
-      {challenge.title}
-    </h3>
-    <p className="challenge-descr body-font">{challenge.description}</p>
-    {!userData?.completedChallenges?.includes(challenge._id) ? <>
-    <p className="challenge-complete body-font">Completed?</p>
-    <button name="completedChallenges" id={challenge._id} onClick={addComplete}>
-      &#10003;
-    </button>
-    </> :
+  return (
     <>
-    <h1>YOU DID IT ALREADY</h1>
+      <div className="challenge-block" key={challenge._id}>
+        <img
+          className="challenge-picture"
+          src="https://res.cloudinary.com/dpsymdmyi/image/upload/v1692365554/bebpthuftxhz18roqbwl.png"
+        />
+        <h3 className="h3-challenge h3-header kindr-header">
+          {challenge.title}
+        </h3>
+        <p className="challenge-descr body-font">{challenge.description}</p>
+        {!userData?.completedChallenges?.includes(challenge._id) ? (
+          <>
+            <p className="challenge-complete body-font">Completed?</p>
+            <button
+              name="completedChallenges"
+              id={challenge._id}
+              onClick={addComplete}
+            >
+              &#10003;
+            </button>
+          </>
+        ) : (
+          <>
+            <h1>YOU DID IT ALREADY</h1>
+          </>
+        )}
+      </div>
+
+      <div className="completed-users-ctr">
+        {completedUsers.length ? (
+          completedUsers.map((user) => {
+            return (
+              <div className="completed-user-card">
+                <h1>{user.username}</h1>
+                <img src={user.picture} alt="" />
+              </div>
+            );
+          })
+        ) : (
+          <h1>Be the first to complete this challenge!</h1>
+        )}
+      </div>
     </>
-}
-  </div>
-    )    
+  );
 }
