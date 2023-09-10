45a94ca6ca38e1d5d7136787d74eed79c118cf43
LouisPino <ljpino95@gmail.com>
2023-09-10 13:10:33 -0400

    testing payload increase

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.urlencoded({ extended: true, limit: "5mb", } ));
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: false, limit: "5mb", } ));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
