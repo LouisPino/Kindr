822330566638906df4c4a989ba3df6b1c4892af1
LouisPino <ljpino95@gmail.com>
2023-09-10 18:04:36 -0400

    nav responds

--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -20,12 +20,12 @@ export default function Header({navScore}) {
   return (
     <header className="header-page">
            <Link to={"/dashboard"}>
-           <h1 className="kindr-header kindr-nav depth">Kindr</h1></Link>
+           <h1 id="kindr-header-title" className="kindr-header kindr-nav depth ">Kindr</h1></Link>
            <div className="user-header">
            <Link to={"/profile"}>
-           <img src="https://res.cloudinary.com/dsvcyich1/image/upload/v1694368090/user-icon_no2nly.png"  />
+           <img className="header-img" src="https://res.cloudinary.com/dsvcyich1/image/upload/v1694368090/user-icon_no2nly.png"  />
             </Link>
-            <h2 className="user-header-score kindr-header kindr-nav depth">{navScore > 0 ? navScore : ''}</h2>
+            <h2 id="user-header-score" className="kindr-header kindr-nav depth">{navScore > 0 ? navScore : ''}</h2>
             </div>
     </header>
   );
