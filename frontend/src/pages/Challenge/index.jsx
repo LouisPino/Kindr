97878135c56b90e847e74bd245a1def1758d016b
LouisPino <ljpino95@gmail.com>
2023-09-11 14:31:03 -0400

    finish cleanup

--- frontend/src/pages/Challenge/index.jsx
+++ frontend/src/pages/Challenge/index.jsx
@@ -1,10 +1,4 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import { useParams } from "react-router";
-import { Link } from "react-router-dom";
 import NewChallengeForm from "../../components/Challenges/NewChallengeForm";
-import { getChallenges } from "../../utilities/challenge-service";
-import { useEffect, useState } from "react";
-import ChallengeList from "../../components/Challenges/ChallengeList";
 import"./challenge.css"
 
 export default function Challenge() {
--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -1,27 +1,22 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
-import { Link, useNavigate } from "react-router-dom";
-import { findUserByEmail, updateUser } from "../../utilities/user-service";
+import { useNavigate } from "react-router-dom";
+import { findUserByEmail } from "../../utilities/user-service";
 import { getChallenges } from "../../utilities/challenge-service";
 import ChallengeList from "../../components/Challenges/ChallengeList";
-
-
-import "./dashboard.css"
 import DailyChallenge from "../../components/Challenges/DailyChallenge";
+import "./dashboard.css"
 
 export default function Dashboard({setNavScore, setOpen}) {
+  //general state
   const { user } = useAuth0();
-  const { userId } = useParams();
   const [isLoading, setIsLoading] = useState(true);
-  const navigate = useNavigate();
-
   const [challenges, setChallenges] = useState(null);
   const [dailyChallenge, setDailyChallenge] = useState();
-
-
   const [userData, setUserData] = useState({})
+  const navigate = useNavigate();
   
+  //grab user data from db
   useEffect(()=>{
     if(user){
     async function fillUserObj(){
@@ -38,14 +33,15 @@ export default function Dashboard({setNavScore, setOpen}) {
   }, [])
 
 
-
+//index challenges on render
   async function handleRequest() {
     const challengeResponse = await getChallenges();
-
     if (challengeResponse.length) {
       setChallenges(challengeResponse);
     }
   }
+
+  //find daily challenge, finish loading
   useEffect(() => {
     if (challenges) {
       const dailyidx = challenges.findIndex((chal)=>chal.daily === true)
@@ -55,7 +51,6 @@ export default function Dashboard({setNavScore, setOpen}) {
   }, [challenges]);
 
   
-
   return isLoading ? (
     <>
       <h1 className="loading">LOADING...</h1>
