096e35a28fbd8bf526a2afaf692f56ec3d58cb73
LouisPino <ljpino95@gmail.com>
2023-09-23 16:32:58 -0400

    return cors setting

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
-  origin: '*',
+  origin: FRONTENDURL,
   methods: "GET, POST, PUT, DELETE",
   credentials: true,
 }))
