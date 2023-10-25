c8e0f7e2737e078071425af4ef381c81c1c65238
LouisPino <ljpino95@gmail.com>
2023-10-25 15:08:43 -0400

    login page mobile styled

--- frontend/src/pages/Welcome/index.jsx
+++ frontend/src/pages/Welcome/index.jsx
@@ -5,27 +5,27 @@ import Sidenav from "../../components/Nav/sidebar";
 import("./welcome.css");
 
 export default function Welcome() {
-const { loginWithRedirect, user } = useAuth0();
-const navigate = useNavigate()
+  const { loginWithRedirect, user } = useAuth0();
+  const navigate = useNavigate()
 
-//redirect on login
-if(user){
-navigate("/dashboard")
-}
+  //redirect on login
+  if (user) {
+    navigate("/dashboard")
+  }
 
   return (
     <>
       <section className="welcome-page">
         <div className="tagline">
-        <h2 className="welcome-header">A good app.&nbsp;</h2>
-        <h2 className="welcome-header">A&nbsp;<div className="very">VERY</div>&nbsp;good app.</h2>
+          <h2 className="welcome-header">A good app.&nbsp;</h2>
+          <h2 className="welcome-header">A&nbsp;<span className="very">VERY</span>&nbsp;good app.</h2>
         </div>
         <p className="body-font welcome-emphasize">
           Welcome!
         </p>
         <p className="body-font welcome-body">
-        We're so glad you're here.</p> <p className="body-font welcome-body">Kindr is an app where you can do good things and inspire others to do
-          good. We're just a bunch of do-gooders 'round these parts.
+          We're so glad you're here.</p> <p className="body-font welcome-body">Kindr is an app where you can do good things and inspire others to do
+            good. We're just a bunch of do-gooders 'round these parts.
         </p>
 
         <button className="viewchallenge-button body-font login" onClick={() => loginWithRedirect()}>LOGIN</button>
