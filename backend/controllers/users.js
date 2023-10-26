e163ebd99e6ccda451f4a3018062b55aacd934d9
laurhimms <lauriehimmel1@gmail.com>
2023-10-25 20:27:34 -0400

    test backend

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -56,7 +56,7 @@ async function updateUser(req, res) {
       }
     }
 
-    const userUpdate = await User.findOneAndUpdate({ email: req.body.email }, data)
+    const userUpdate = await User.findOneAndUpdate({ email: req.body.email }, ...data)
 
     res.status(200).json({
       success: true,
